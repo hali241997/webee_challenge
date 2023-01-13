cc391fe550e904f495e411ae19330689279addbf
Hasnain Ali <mhali241997@gmail.com>
2023-01-14 04:16:00 +0500

    Setup linting

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Text, View } from "react-native";
+
+const TextField: FC = () => {
+  return (
+    <View>
+      <Text>TextField</Text>
+    </View>
+  );
+};
+
+export default TextField;
