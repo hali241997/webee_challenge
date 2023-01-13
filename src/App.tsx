eabc653958f9f8f7218c175b247933b23cea76ba
Hasnain Ali <mhali241997@gmail.com>
2023-01-14 04:51:48 +0500

    Add react-native-paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React, { FC } from "react";
 import { SafeAreaView, Text } from "react-native";
+import {
+  ActivityIndicator,
+  Provider as PaperProvider,
+} from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const App: FC = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <Text>Hello</Text>
-      </SafeAreaView>
+      <PaperProvider>
+        <SafeAreaView>
+          <Text>Hello</Text>
+          <Icon name="home" size={20} />
+          <ActivityIndicator animating />
+        </SafeAreaView>
+      </PaperProvider>
     </NavigationContainer>
   );
 };
